fc262b0c9bd2020a367cc672b89e2218dc552b3f
felipeghidini <lipeghidini18@gmail.com>
2022-12-21 06:27:36 -0300

    primeiros testes unitarios

---
+++ notifications-service/src/application/entities/content.spec.ts
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+import { Content } from "./content";
+
+describe('Notification content', () => {
+    it("should be able to create a notification content", () => {
+        const content = new Content("Voce recebeu uma solicitação de amizade.");
+    
+        expect(content).toBeTruthy();
+    });
+    
+    it("should not be able to create a notification content with less than 5 characters", () => {
+    
+        expect(() => new Content('sim')).toThrow();
+    });
+    
+    it("should not be able to create a notification content with more than 240 characters", () => {
+    
+        expect(() => new Content('a'.repeat(241))).toThrow();
+    });
+
+})
+
